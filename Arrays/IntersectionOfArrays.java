f2399cdc111f4e70a4329f489da2cb047875207d
Likhitha <blikhithavenkatasaisri@gmail.com>
2026-05-23 16:26:46 +0530

    Added array problems

--- Arrays/IntersectionOfArrays.java
+++ Arrays/IntersectionOfArrays.java
@@ -10,3 +10,9 @@ class Solution {
         return res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+
+// Problem: Intersection of Two Arrays
+// Problem Link: https://leetcode.com/problems/intersection-of-two-arrays/description/
+// Approach: Using HashSet to store elements of the first array and then checking for common elements in the second array.
+// Time Complexity: O(n + m) where n and m are the lengths of the two input arrays, as we traverse both arrays once.
+// Space Complexity: O(n) where n is the length of the first input array, as we store its elements in a HashSet. The space used for the result set is at most O(min(n, m)) where m is the length of the second input array.
