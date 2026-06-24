16310e2b556c9c6cd0b426396536cd40405a18c4
Likhitha Venkata Sai Sri Bethu <blikhithavenkatasaisri@gmail.com>
2026-06-24 14:13:48 +0530

    Add Backspace String Compare solution

--- Stack/BackspaceStringCompare.java
+++ Stack/BackspaceStringCompare.java
@@ -4,24 +4,28 @@ class Solution {
         Stack<Character> stk2 = new Stack<>();
         int i = 0;
         while (i < s.length()) {
-            if (!stk1.isEmpty() && s.charAt(i) == '#')
-                stk1.pop();
+            if (s.charAt(i) == '#'){
+                if (!stk1.isEmpty())
+                stk1.pop();}
             else
                 stk1.push(s.charAt(i));
             i++;
         }
         i = 0;
         while (i < t.length()) {
-            if (!stk2.isEmpty() && t.charAt(i) == '#')
-                stk2.pop();
+            if (t.charAt(i) == '#'){
+                if (!stk2.isEmpty())
+                stk2.pop();}
             else
                 stk2.push(t.charAt(i));
             i++;
         }
-        while (!stk1.isEmpty()) {
+        while (!stk1.isEmpty() && !stk2.isEmpty()) {
             if (stk1.pop() != stk2.pop())
                 return false;
         }
+        if ((stk1.isEmpty() && !stk2.isEmpty()) || (!stk1.isEmpty() && stk2.isEmpty()))
+            return false;
         return true;
     }
 }
